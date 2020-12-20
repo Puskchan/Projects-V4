0e2f7f5ca58a87eb12b7965321c2e346e6b4236c
Aditya Chandhoke <50488410+Puskchan@users.noreply.github.com>
2020-12-20 12:00:35 +0000

    Add files via upload

---
+++ 2nd_project/madlibs.py
@@ -0,0 +1,8 @@
+adj = input("Adjective : ")
+adj2 = input("Adjective : ")
+verb = input("Verb : ")
+verb2 = input("Verb : ")
+person = input("Any Name : ")
+madlib = f"So, hey good {adj}, how's life? How do you spend your day with that {adj2} mental??. \n I just love to go " \
+         f"and {verb}, although it would be nice if I could do {verb2} and be {person}. "
+print(madlib)
